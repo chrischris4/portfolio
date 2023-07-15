84526a3412298f94e5adce3980b278b48bb55c34
Jost Christopher <jostchristopher44@gmail.com>
2023-07-15 03:21:27 +0200

    responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,16 +3,18 @@ import '../styles/Banner.css';
 function Banner() {
     return (
         <div className="bannerAll">
-            <div className="banner">
+            <div className="bannerColor">
                 <div className="bannerLastName">JOST</div>
             </div>
-            <div className="bannerBackground">
-                <div className="bannerFirstName">CHRISTOPHER</div>
-                <div className="bannerP"> Développeur FullStack</div>
-                <div className="bannerAbout">
-                    Développeur spécialisé dans la création d'applications web
-                    dynamiques. Prêt à relever de nouveaux défis et à contribuer
-                    à des projets innovants !
+            <div className="banner">
+                <div className="bannerContent">
+                    <div className="bannerFirstName">CHRISTOPHER</div>
+                    <div className="bannerP"> Développeur FullStack</div>
+                    <div className="bannerAbout">
+                        Développeur spécialisé dans la création d'applications
+                        web dynamiques. Prêt à relever de nouveaux défis et à
+                        contribuer à des projets innovants !
+                    </div>
                 </div>
             </div>
         </div>
