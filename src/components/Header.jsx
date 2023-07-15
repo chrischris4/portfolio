68f58265ad7b920a18c500b6eda897fc718bb1f5
Jost Christopher <jostchristopher44@gmail.com>
2023-07-16 00:44:50 +0200

    add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,22 @@ import '../styles/Header.css';
 import linkedin from '../assets/linkedin.png';
 import git from '../assets/git.png';
 import vectorUp from '../assets/vectorUp.jpg';
-// import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import backHome from '../assets/backHome.png';
 
 function Header() {
-    // const location = useLocation();
-    // const getHeaderBackground = () => {
-    //     if (location.pathname === '/') {
-    //         return 'header';
-    //     } else if (
-    //         location.pathname.startsWith('/Kasa') ||
-    //         location.pathname.startsWith('/LeVieuxGrimoire') ||
-    //         location.pathname.startsWith('/NinaCarducci') ||
-    //         location.pathname.startsWith('/SophieBluel')
-    //     ) {
-    //         return 'headerProject';
-    //     }
-    // };
+    const location = useLocation();
+
+    const linksHome = location.pathname === '/';
+    const allLinksPages = [
+        '/Kasa',
+        '/LeVieuxGrimoire',
+        '/NinaCarducci',
+        '/SophieBluel',
+    ];
+    const linksPage = allLinksPages.includes(location.pathname);
+
     window.addEventListener('scroll', function () {
         var topPage = document.querySelector('.topPage');
 
@@ -29,20 +29,35 @@ function Header() {
     });
 
     return (
-        <div className={'header'}>
+        <div className="header">
             <div className="profil">
                 <a href="https://www.linkedin.com/in/christopher-jost-888b75195/">
                     <img className="linkedin" src={linkedin} alt="" />
                 </a>{' '}
                 <img className="github" src={git} alt="" />
+                {linksPage && (
+                    <>
+                        <Link className="backHomeLink" to={`/`}>
+                            <img
+                                className="backHomeImg"
+                                src={backHome}
+                                alt=""
+                            />
+                        </Link>
+                    </>
+                )}
             </div>
             <nav>
-                <a href="#competenceLien" className="link">
-                    Compétences
-                </a>
-                <a href="#projectLien" className="link">
-                    Mes projets
-                </a>
+                {linksHome && (
+                    <>
+                        <a href="#competenceLien" className="link">
+                            Compétences
+                        </a>
+                        <a href="#projectLien" className="link">
+                            Mes projets
+                        </a>
+                    </>
+                )}
                 <a href="#topPageLink" className="topPage">
                     <img className="vectorUp" src={vectorUp} alt="" />
                 </a>
