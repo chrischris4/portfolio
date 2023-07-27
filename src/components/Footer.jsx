fa46774e93eae70f0ab7831ca18e4b0e465d2472
Jost Christopher <jostchristopher44@gmail.com>
2023-07-27 22:31:43 +0200

    style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,19 @@ import '../styles/Footer.css';
 function Footer() {
     return (
         <div className="footer">
+            <p className="pFooter">
+                Ce site a été réalisé par mes soins, vous pouvez retrouvez son
+                code sur
+                <a
+                    className="linkFooter"
+                    href="https://github.com/chrischris4/p8"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    GitHub
+                </a>
+                . Merci de votre interêt.
+            </p>
             <p className="pFooter">© 2023 by JOST Christopher</p>
         </div>
     );
