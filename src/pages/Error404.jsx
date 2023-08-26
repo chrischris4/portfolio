e0a1aaf96eff25b8a4f31db7583a97821677336c
Jost Christopher <jostchristopher44@gmail.com>
2023-08-26 03:30:55 +0200

    add

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,9 +1,11 @@
 import '../styles/Error404.css';
 import { Link } from 'react-router-dom';
-import { useEffect, useContext, useState } from 'react';
+import { useEffect, useContext, useState, useRef, useMemo } from 'react';
 import { ThemeContext } from '../components/ThemeSombre';
+import { useTranslation } from 'react-i18next';
 
 function Error404() {
+    const { t } = useTranslation();
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
@@ -24,12 +26,40 @@ function Error404() {
         localStorage.setItem('theme', theme);
     }, [theme]);
 
+    const images = useMemo(
+        () => [
+            'https://i.ibb.co/9rt0cYk/server-control-6688463.png',
+            'https://i.ibb.co/42hGMc1/error-404-9723235.png',
+            'https://i.ibb.co/fCRFBR5/skull-bones-5354410.png',
+        ],
+        []
+    );
+
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex(
+                (prevIndex) => (prevIndex + 1) % images.length
+            );
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [images]);
+
+    useEffect(() => {
+        imageRef.current.src = images[currentImageIndex];
+    }, [currentImageIndex, images]);
+
     return (
         <div className={`error${theme === 'dark' ? ' night' : ''}`}>
-            <h2 className="error404">404</h2>
-            <p className="errorP">La page que vous demandez n'existe pas.</p>
+            <img className="error404" ref={imageRef} alt="" />
+            <p className="errorP">{t('errorP')}</p>
             <Link to="/" className="errorLink">
-                Retourner sur la page d'Accueil
+                {t('errorLink')}
             </Link>
         </div>
     );
